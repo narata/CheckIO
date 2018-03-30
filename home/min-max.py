2dd3e75ea9cc105ba356785d62f57d298b3609e6
mac <mac@narata.com>
2018-03-30 23:23:18 +0800

    edit home:min-max good

--- home/min-max.py
+++ home/min-max.py
@@ -1,15 +1,16 @@
-from collections import Iterable
+import collections
 
 
 def min(*args, **kwargs):
     args = args[0] if len(args) == 1 else args
+    if not isinstance(args, collections.Iterable):
+        return args
+    args = [buf for buf in args]
     key = kwargs.get("key", None)
     if key is not None:
         args_later = [key(buf) for buf in args]
     else:
         args_later = args
-    if not isinstance(args, Iterable):
-        return args
     result = args[0]
     result_later = args_later[0]
     for i in range(1, len(args)):
@@ -21,17 +22,17 @@ def min(*args, **kwargs):
 
 def max(*args, **kwargs):
     args = args[0] if len(args) == 1 else args
+    if not isinstance(args, collections.Iterable):
+        return args
+    args = [buf for buf in args]
     key = kwargs.get("key", None)
     if key is not None:
         args_later = [key(buf) for buf in args]
     else:
         args_later = args
-    if not isinstance(args, Iterable):
-        return args
-    i = 0
-    for buf in args_later:
-        if i == 0:
-        
+    result = args[0]
+    result_later = args_later[0]
+    for i in range(1, len(args)):
         if args_later[i] > result_later:
             result = args[i]
             result_later = args_later[i]
@@ -39,11 +40,10 @@ def max(*args, **kwargs):
 
 
 if __name__ == '__main__':
-    # assert max(3, 2) == 3, "Simple case max"
-    # assert min(3, 2) == 2, "Simple case min"
-    # assert max([1, 2, 0, 3, 4]) == 4, "From a list"
-    # assert min("hello") == "e", "From string"
-    # assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
-    # assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
-    # print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
-    min(abs(i) for i in range(-10, 10))
+    assert max(3, 2) == 3, "Simple case max"
+    assert min(3, 2) == 2, "Simple case min"
+    assert max([1, 2, 0, 3, 4]) == 4, "From a list"
+    assert min("hello") == "e", "From string"
+    assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
+    assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
